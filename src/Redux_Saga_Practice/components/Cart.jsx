878a9d6da780b8a9073eadc1c01280133272df2c
pavank0723 <pavan.yadav0723@gmail.com>
2022-11-27 21:48:37 +0530

    display cart product

--- src/Redux_Saga_Practice/components/Cart.jsx
+++ src/Redux_Saga_Practice/components/Cart.jsx
@@ -1,7 +1,52 @@
-const Cart = () =>{
-    return(
+import { useSelector } from 'react-redux'
+import '../style.css'
+
+const Cart = () => {
+    const data = useSelector((state) => state.cartData)
+    console.warn('Cart data ==>>>',data)
+    return (
         <>
-        <h1>Cart Page</h1>
+            <div className='cart_header'>
+                <h2>Cart Page</h2>
+                <h3>2 Items</h3>
+            </div>
+            <div className='cart_container'>
+                <table className="cart_data" border={{ border: "1px solid grey" }}>
+                    <thead>
+                        <tr>
+                            <th>Product</th>
+                            <th colSpan='3'>Qty</th>
+                            <th>Price</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            data.map((item) => {
+                                const { id, name, photo, price } = item
+                                return (
+                                    <tr key={id}>
+                                        <td className='cart_product_data'>
+                                            <img src={photo} alt={`product ${id}`} />
+                                            {name}
+                                        </td>
+                                        <td>
+                                            <button>-</button>
+                                        </td>
+                                        <td>1</td>
+                                        <td>
+                                            <button>+</button>
+                                        </td>
+                                        <td>₹{price}</td>
+                                        <td>₹1000</td>
+                                    </tr>
+                                )
+                            })
+                        }
+
+                    </tbody>
+                </table>
+            </div>
 
         </>
     )
